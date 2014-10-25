03ca54b9987d201894403277f480fe5f01880c9e
Andrew <andrewbranch@mail.com>
2014-10-25 11:42:25 -0500

    See what NODE_ENV is on heroku

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ server.configure(function() {
 require('./app')(server);
 require('./server')(server);
 
+console.log(NODE_ENV);
 if (NODE_ENV === 'production') {
 	server.listen(process.env.PORT || 8001);
 } else {
