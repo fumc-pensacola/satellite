5e96d45cfbbb89c0748ac16bcbb184783ffb6db5
Andrew Branch <andrewbranch@mail.com>
2014-12-19 16:01:35 -0600

    Allow fumcpensacola.com to access API

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var forceSSL = function (req, res, next) {
 };
 
 var allowCrossDomain = function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'fumcpensacola.com');
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	next();
