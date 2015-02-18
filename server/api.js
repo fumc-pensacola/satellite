d88495f2cc4f4ff84e20408f3cd1b3b3f626856b
Andrew Branch <andrewbranch@mail.com>
2015-02-17 19:06:31 -0800

    Fix #18, missing file API endpoint

--- server/api.js
+++ server/api.js
@@ -6,6 +6,7 @@ module.exports = function (server) {
 	require('./controllers/calendars')(server, new ACS());
 	require('./controllers/notifications')(server);
 	require('./controllers/url')(server);
+	require('./controllers/file')(server);
 	require('./controllers/orm')(server);
 
 };
