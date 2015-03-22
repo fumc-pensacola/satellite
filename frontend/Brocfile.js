6cb79c05eb0cf24133590a74101e5a28e526e82b
Andrew Branch <andrewbranch@mail.com>
2015-03-22 12:31:29 -0700

    Upgrade Ember

--- frontend/Brocfile.js
+++ frontend/Brocfile.js
@@ -4,7 +4,11 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app'),
     pickFiles = require('broccoli-static-compiler'),
     mergeTrees = require('broccoli-merge-trees');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  vendorFiles: {
+    'handlebars.js': null
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
