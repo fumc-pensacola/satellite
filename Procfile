c7946adf1497e1d213bf2ca533a0d997e872b669
Andrew Branch <andrewbranch@mail.com>
2015-03-29 13:00:00 -0700

    Upgrade and reconfigure server for new structure

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: node server/index.js
-dev: node server/index.js & (ember server)
-debug: node-debug -p 3000 server/index.js
+dev: ember build --environment=development && node server/index.js
+debug: ember build --environment=development && node-debug -p 3000 server/index.js
--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
     orm = require('orm'),
     NODE_ENV = process.env.NODE_ENV,
     DATABASE_URL = process.env.DATABASE_URL + '?ssl=true';
@@ -19,34 +20,32 @@ module.exports = function (server) {
     next();
   };
 
-  server.configure(function() {
+  // I don't think we have any application/x-www-form-urlencoded requests
+  // server.use(bodyParser.urlencoded({ extended: false }));
+  server.use(bodyParser.json());
+  server.use(allowCrossDomain);
+  if (NODE_ENV === 'production') {
+    server.use(forceSSL);
+  }
 
-    server.use(express.json());
-    server.use(express.urlencoded());
-    server.use(allowCrossDomain);
-    if (NODE_ENV === 'production') {
-      server.use(forceSSL);
+  server.use(orm.express(DATABASE_URL, {
+    define: function (db, models, next) {
+
+      models.bulletin = require('./models/bulletin')(db);
+      models.witness = require('./models/witness')(db);
+      models.setting = require('./models/setting')(db);
+      models.feature = require('./models/feature')(db);
+      models.notification = require('./models/notification')(db);
+      models.calendar = require('./models/calendar')(db);
+
+      models.notification.hasOne('feature', models.feature);
+
+      db.settings.set('instance.returnAllErrors', true);
+      // db.drop();
+      db.sync();
+
+      next();
     }
-
-    server.use(orm.express(DATABASE_URL, {
-      define: function (db, models, next) {
-
-        models.bulletin = require('./models/bulletin')(db);
-        models.witness = require('./models/witness')(db);
-        models.setting = require('./models/setting')(db);
-        models.feature = require('./models/feature')(db);
-        models.notification = require('./models/notification')(db);
-        models.calendar = require('./models/calendar')(db);
-
-        models.notification.hasOne('feature', models.feature);
-
-        db.settings.set('instance.returnAllErrors', true);
-        // db.drop();
-        db.sync();
-
-        next();
-      }
-    }));
-  });
+  }));
 
 };
