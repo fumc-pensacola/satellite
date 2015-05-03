949c360db39d12b8c9cf0141d9b87dd57b9f98f3
Andrew Branch <andrewbranch@mail.com>
2015-05-03 15:11:47 -0700

    Fingerprint and cache S3 files. Closes fumc-pensacola/transept#41.

--- api/controllers/file.js
+++ api/controllers/file.js
@@ -1,15 +1,5 @@
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3({ params: { Bucket: 'fumcappfiles' } });
-
 var _get = function (req, res) {
-  s3.getSignedUrl('getObject', { Key: req.params.key }, function (err, url) {
-    if (err) {
-      console.error(err);
-      res.status(500).send(err);
-    } else {
-      res.redirect(303, url);
-    }
-  });
+  res.redirect(303, 'https://s3.amazonaws.com/fumcappfiles/' + req.params.key);
 };
 
 var _exports = function (router) {
