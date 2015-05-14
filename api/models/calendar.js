59d3e92a24c305bfc521554bcc2f31dae074f3b9
Andrew Branch <andrewbranch@mail.com>
2015-05-13 20:39:38 -0700

    Add _id to schema

--- api/models/calendar.js
+++ api/models/calendar.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Calendar', new mongoose.Schema({
+  _id: { type: String, required: true, index: { unique: true } },
   name: { type: String, required: true },
   color: { type: String },
   image: { type: String }
