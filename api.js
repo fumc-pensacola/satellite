dda98d1129563deb09dc7eb1b6ae2852a85397b2
Andrew Branch <andrewbranch@mail.com>
2015-05-31 14:32:23 -0700

    Make API v3 for JSON API 1.0

--- api.js
+++ api.js
@@ -9,29 +9,38 @@ let express = require('express'),
     notificationsController = require('./controllers/notifications'),
     notificationsControllerV1 = require('./v1/controllers/notifications'),
     ormController = require('./controllers/orm'),
-    ormControllerV1 = require('./v1/controllers/orm');
+    ormControllerV1 = require('./v1/controllers/orm'),
+    ormControllerV2 = require('./v2/controllers/orm');
     
 
 module.exports = function(server) {
 
   let v1 = express.Router(),
-      v2 = express.Router();
+      v2 = express.Router(),
+      v3 = express.Router();
   
   // Shared controllers
-  [v1, v2].forEach(v => {
+  [v1, v2, v3].forEach(v => {
     authenticationController(v);
     emailerController(v);
     fileController(v);
   });
   
+  let routeBase = {
+    v1: '(/api)?(/v1)?',
+    v2: '(/api)?/v2',
+    v3: '/v3'
+  };
+  
   // Version-specific controllers
-  let routeBase = '(/api)?/v2';
   ormControllerV1(v1);
-  ormController(v2, routeBase);
+  ormControllerV2(v2, routeBase.v2);
+  ormController(v3, routeBase.v3);
   notificationsControllerV1(v1);
   notificationsController(v2);
   calendarsController(v1, new ACS());
 
-  server.use('(/api)?(/v1)?', v1);
-  server.use(routeBase, v2);
+  server.use(routeBase.v1, v1);
+  server.use(routeBase.v2, v2);
+  server.use(routeBase.v3, v3)
 };
