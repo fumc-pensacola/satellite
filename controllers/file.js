1e7817a89bd4642ea6806240790b068f056b8fe9
Andrew Branch <andrewbranch@mail.com>
2015-05-31 13:43:58 -0700

    Don't do that silly thing for sharing with Ember mocks

--- controllers/file.js
+++ controllers/file.js
@@ -1,15 +1,7 @@
 "use strict";
 
-let _get = (req, res) => {
-  res.redirect(303, 'https://s3.amazonaws.com/fumcappfiles/' + req.params.key);
+module.exports = function(router) {
+  router.get('/file/:key', (req, res) => {
+    res.redirect(303, 'https://s3.amazonaws.com/fumcappfiles/' + req.params.key);
+  });
 };
-
-let _exports = function(router) {
-  router.get('/file/:key', _get);
-};
-
-// The Ember http-mock can use the exact same function,
-// so we want to expose it here for importing.
-_exports._get = _get;
-
-module.exports = _exports;
