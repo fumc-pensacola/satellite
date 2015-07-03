c438e6bfc0f7403620d68a99e7bbf2acf9d91aca
Andrew Branch <andrewbranch@mail.com>
2015-07-02 19:23:39 -0700

    Rename lectionary to description

--- models/bulletin.js
+++ models/bulletin.js
@@ -5,7 +5,7 @@ let mongoose = require('mongoose');
 module.exports = mongoose.model('Bulletin', new mongoose.Schema({
   date: { type: Date, required: true },
   service: { type: String, required: true },
-  lectionary: { type: String },
+  description: { type: String },
   liturgicalDay: { type: String, required: true },
   visible: { type: Boolean },
   file: { type: String },
